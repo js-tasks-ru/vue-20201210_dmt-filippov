fd984c09bdb6883ff356bec9ca2ebaa191ee578b
Dmitriy Filippov <dmt.filippov@gmail.com>
2020-12-21 01:13:59 +0700

    add solution for 01-basics 02-MeetupPageDraft

--- 01-basics/02-MeetupPageDraft/script.js
+++ 01-basics/02-MeetupPageDraft/script.js
@@ -1,7 +1,7 @@
-import Vue from './vue.esm.browser.js';
+import Vue from "./vue.esm.browser.js";
 
 /** URL адрес API */
-const API_URL = 'https://course-vue.javascript.ru/api';
+const API_URL = "https://course-vue.javascript.ru/api";
 
 /** ID митапа для примера; используйте его при получении митапа */
 const MEETUP_ID = 6;
@@ -16,17 +16,29 @@ function getMeetupCoverLink(meetup) {
 }
 
 /**
+ * Возвращает строку даты в формате YYYY-MM-DD
+ * @param date {Date} - Дата
+ * @return {string} - дата в формате YYYY-MM-DD
+ */
+const getDateOnlyString = (date) => {
+  const YYYY = date.getUTCFullYear();
+  const MM = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+  const DD = date.getUTCDate().toString().padStart(2, "0");
+  return `${YYYY}-${MM}-${DD}`;
+};
+
+/**
  * Словарь заголовков по умолчанию для всех типов элементов программы
  */
 const agendaItemTitles = {
-  registration: 'Регистрация',
-  opening: 'Открытие',
-  break: 'Перерыв',
-  coffee: 'Coffee Break',
-  closing: 'Закрытие',
-  afterparty: 'Afterparty',
-  talk: 'Доклад',
-  other: 'Другое',
+  registration: "Регистрация",
+  opening: "Открытие",
+  break: "Перерыв",
+  coffee: "Coffee Break",
+  closing: "Закрытие",
+  afterparty: "Afterparty",
+  talk: "Доклад",
+  other: "Другое"
 };
 
 /**
@@ -34,33 +46,64 @@ const agendaItemTitles = {
  * Соответствует имени иконок в директории /assets/icons
  */
 const agendaItemIcons = {
-  registration: 'key',
-  opening: 'cal-sm',
-  talk: 'tv',
-  break: 'clock',
-  coffee: 'coffee',
-  closing: 'key',
-  afterparty: 'cal-sm',
-  other: 'cal-sm',
+  registration: "key",
+  opening: "cal-sm",
+  talk: "tv",
+  break: "clock",
+  coffee: "coffee",
+  closing: "key",
+  afterparty: "cal-sm",
+  other: "cal-sm"
 };
 
 export const app = new Vue({
-  el: '#app',
+  el: "#app",
 
   data: {
-    //
+    rawMeetupData: null
   },
 
   mounted() {
     // Требуется получить данные митапа с API
+    this.getMeetupData(MEETUP_ID);
   },
 
   computed: {
-    //
+    meetupData: function() {
+      if (!this.rawMeetupData) return null;
+      return {
+        ...this.rawMeetupData,
+        coverStyle: this.rawMeetupData.imageId
+          ? {
+            "--bg-url": `url('${getMeetupCoverLink(this.rawMeetupData)}')`
+          }
+          : {},
+        localDate: new Date(this.rawMeetupData.date).toLocaleString(navigator.language, {
+          year: "numeric",
+          month: "short",
+          day: "numeric"
+        }),
+        dateOnlyString: getDateOnlyString(new Date(this.rawMeetupData.date))
+      };
+    },
+    agenda: function() {
+      if (!this.rawMeetupData) return null;
+      return this.rawMeetupData.agenda.map(section => {
+        return {
+          ...section,
+          title: section.title ? section.title : agendaItemTitles[section.type],
+          icon: section.type ? agendaItemIcons[section.type] : agendaItemIcons.other
+        };
+      });
+    },
+    hasAgenda: function() {
+      return !!this.rawMeetupData.agenda.length;
+    }
   },
 
   methods: {
-    // Получение данных с API предпочтительнее оформить отдельным методом,
-    // а не писать прямо в mounted()
-  },
+    async getMeetupData(meetupId) {
+      this.rawMeetupData = await fetch(`${API_URL}/meetups/${meetupId}`).then((res) => res.json());
+    }
+  }
 });
